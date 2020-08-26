aa3c6570cac9b0db1ebd13b771646dc32ae66b2b
korina-pluss27 <korina.pluss@gmail.com>
2020-08-26 00:06:39 -0400

    :dizzy: remove debounce from scroll listener

--- src/hooks/useSticky.js
+++ src/hooks/useSticky.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 
 const useSticky = () => {
   const [isSticky, setSticky] = useState(true);
@@ -12,28 +12,12 @@ const useSticky = () => {
     previousScroll = currentScroll;
   };
 
-  const debounce = (func, wait = 20, immediate = true) => {
-    let timeOut;
-    return () => {
-      let context = this,
-        args = arguments;
-      const later = () => {
-        timeOut = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeOut;
-      clearTimeout(timeOut);
-      timeOut = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
-
   useEffect(() => {
-    window.addEventListener('scroll', debounce(handleScroll));
+    window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', () => handleScroll);
     };
-  }, [debounce, handleScroll]);
+  }, [handleScroll]);
 
   return { isSticky };
 };
