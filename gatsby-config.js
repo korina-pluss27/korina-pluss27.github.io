2adc32b052e8637893caae92cc06c96b8b21205a
korina-pluss27 <korina.pluss@gmail.com>
2020-08-26 00:03:56 -0400

    :construction: fix project config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Korina Pluss`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `The portfolio website for Korina Pluss.`,
+    author: `Korina Pluss`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
