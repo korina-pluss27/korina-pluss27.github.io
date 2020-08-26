eaa110d4ce5edf854bfed04f7a698587a7830bee
korina-pluss27 <korina.pluss@gmail.com>
2020-08-26 00:09:46 -0400

    :recycle: move contact section into its own dir

---
+++ src/components/contact/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import GitHub from './github';
+import LinkedIn from './linkedin';
+import Mail from './mail';
+import WordsSection from '../../components/wordsSection';
+import { Flex, Box } from '../styled';
+
+const SocialBar = () => {
+  return (
+    <Flex marginY={4} justifyContent="center">
+      <Box marginRight={4} />
+      <GitHub />
+      <Box marginRight={4} />
+      <LinkedIn />
+      <Box marginRight={4} />
+      <Mail />
+      <Box marginRight={4} />
+    </Flex>
+  );
+};
+
+const Contact = () => {
+  const words =
+    'Get in touch with me! I am currently searching for a co-op for the January-June 2021 time period.';
+  return (
+    <>
+      <SocialBar />
+      <Box width="80%">
+        <WordsSection title="Contact" words={words} />
+      </Box>
+    </>
+  );
+};
+
+export default Contact;
