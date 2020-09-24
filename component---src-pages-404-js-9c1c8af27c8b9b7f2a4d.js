29db29bf3594f6a9e22baa8f50720080f577cf2c
korina-pluss27 <korina.pluss@gmail.com>
2020-09-24 16:03:09 -0400

    Updates

---
+++ component---src-pages-404-js-9c1c8af27c8b9b7f2a4d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(t,n,e){"use strict";e.r(n);var a=e("q1tI"),u=e.n(a),o=e("vrFN"),s=e("qKvR");n.default=function(){return Object(s.a)(u.a.Fragment,null,Object(s.a)(o.a,{title:"404: Not found"}),Object(s.a)("h1",null,"NOT FOUND"),Object(s.a)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9c1c8af27c8b9b7f2a4d.js.map
