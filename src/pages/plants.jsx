e9f868ada192cfedcb170e7ab99cce7a8313555f
korina-pluss27 <korina.pluss@gmail.com>
2020-09-28 20:27:54 -0400

    :farmer: add seo and favicon for deploy

--- src/pages/plants.jsx
+++ src/pages/plants.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import PlantsPics from '../components/plantsPics';
+import SEO from '../components/seo';
 import { Flex, Text } from '../components/styled';
 import '../styles/index.css';
 
 const Plants = () => {
   return (
     <main id="kp-main">
+      <SEO title="Korina Pluss" />
       <Flex flexDirection="column" alignItems="center">
         <Text color="#ebe8ea" margin={4} as="h1">
           My Plants
