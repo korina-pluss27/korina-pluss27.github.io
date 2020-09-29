a2b9d1e309dbb24fcd6e39685f15266f24cb019d
korina-pluss27 <korina.pluss@gmail.com>
2020-09-28 20:28:32 -0400

    Updates

---
+++ component---src-pages-404-js-38f499e1fe77acf1cbd5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,n,e){"use strict";e.r(n);var a=e("q1tI"),u=e.n(a),o=e("vrFN"),s=e("qKvR");n.default=function(){return Object(s.a)(u.a.Fragment,null,Object(s.a)(o.a,{title:"404: Not found"}),Object(s.a)("h1",null,"NOT FOUND"),Object(s.a)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-38f499e1fe77acf1cbd5.js.map
